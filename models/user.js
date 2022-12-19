bf7bf5630290fd46f6fd0cd57d82664851195a1d
AdryDevs <alonsoadrian86@gmail.com>
2022-12-19 21:23:02 +0100

    feat: user attributes added, admin user created as user seeder

---
+++ models/user.js
@@ -0,0 +1,59 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      user.hasOne(models.role, { foreignKey: 'id' });
+    }
+  }
+  user.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 20]
+    }
+  }, 
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+    }
+  },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 20]
+    }
+  },
+  id_role:{
+    type:DataTypes.INTEGER
+  },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    }, sequelize,
+    modelName: 'user',
+  });
+  return user;
+};
---
+++ seeders/20221219194956-users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('users', [{
+      username: 'Adrian Alonso',
+      email: 'alonsoadrian86@gmail.com',
+      password: 'adrian',
+      id_role: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
