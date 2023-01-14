e4b6d42ef6c27f7dbb4a9e8e2277bde7ed3fe36c
AdryDevs <alonsoadrian86@gmail.com>
2023-01-14 14:30:56 +0100

    fix: auth for login using try and catch

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,43 +5,44 @@ const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth');
 const { secret } = require('../config/auth');
 
-const AuthController = {};
-
-//Login 
 AuthController.login = async (req, res) => {
-    const { email, password } = req.body;
-    const userFound = await models.user.findOne({ 
-        where: { email } });
-    if (!userFound) {
-        res.status(400).json({ message: 'User or password not valid' });
-    return
-    }
-    //Check password
-    const passCheck = await bcrypt.compare(password, userFound.password);
+    try {
+        const { email, password } = req.body;
+        const userFound = await models.user.findOne({
+            where: { email }
+        });
+        if (!userFound) {
+            throw new Error('User or password not valid');
+        }
+        //Check password
+        const passCheck = await bcrypt.compare(password, userFound.password);
 
-    if (!passCheck) {
-        res.status(400).json({ message: 'Password or user not valid' });
-    return
-    }
-    //Check if JWT secret is defined
-    const secret = process.env.JWT_SECRET;
+        if (!passCheck) {
+            throw new Error('Password or user not valid');
+        }
+        //Check if JWT secret is defined
+        const secret = process.env.JWT_SECRET;
 
-    if (secret.length < 1) {
-        throw new Error('JWT secret not defined');
+        if (secret.length < 1) {
+            throw new Error('JWT secret not defined');
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
+
 
     //Generate token
-        const token = jwt.sign({
-            id: userFound.id,
-            email: userFound.email,
-            created: Date.now(),
-            role: userFound.role
-        }, authConfig.secret, {
-            expiresIn: 86400 
-        });
+    const token = jwt.sign({
+        id: userFound.id,
+        email: userFound.email,
+        created: Date.now(),
+        role: userFound.role
+    }, authConfig.secret, {
+        expiresIn: 86400
+    });
 
-        //Send token and message
-        res.json({ message: 'User logged in', token });
+    //Send token and message
+    res.json({ message: 'User logged in', token });
 };
 
 //Register new user and generate token
@@ -49,14 +50,14 @@ AuthController.register = async (req, res) => {
     const { email, password, username } = req.body;
 
     //Check if user already exists
-    if (await models.user.findOne({ where: { email:email } })) {
-      return res.status(400).send({ error: 'User already exists' });
-  }   
+    if (await models.user.findOne({ where: { email: email } })) {
+        return res.status(400).send({ error: 'User already exists' });
+    }
     //Encrypt password
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 8);
 
-        const user = await models.user.create ({
+        const user = await models.user.create({
             username,
             email,
             id_role: 2,
